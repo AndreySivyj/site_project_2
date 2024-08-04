069b392f168ec9f5bedd04f7c5820efbda45c776
Andrey Sivyj <andreysivyj@gmail.com>
2024-08-05 02:08:48 +0300

    add app account (login and logout)

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +21,7 @@ ALLOWED_HOSTS = [config("HOST_IP"), '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +117,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Вместо имен URL-адресов в этих настроечных параметрах также можно использовать жестко привязанные URL-адреса
+LOGIN_REDIRECT_URL = 'dashboard' # URL-адрес, на который следует перенаправлять пользователя после успешного входа, если в запросе нет параметра next
+LOGIN_URL = 'login' # URL-адрес, на который следует перенаправлять пользователя, чтобы зарегистрировать его вход (например, представления, в которых используется декоратор login_required)
+LOGOUT_URL = 'logout' # URL-адрес, на который следует перенаправлять пользователя, чтобы зарегистрировать его выход
+
