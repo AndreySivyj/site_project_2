b598cfd698505739f6410f1fb4a89872ae2baa7d
Andrey Sivyj <andreysivyj@gmail.com>
2024-08-11 03:49:28 +0300

    User model extension - add Profile model (edit template, forms, view, admin)

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 ]
+
+# вспомогательная функция static(), чтобы раздавать медиафайлы с по­мощью сервера разработки во время разработки (когда DEBUG равен True)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
