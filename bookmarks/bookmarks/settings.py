7ddbdd4d60d1faa241571b2d374c97ed1243dc46
Andrey Sivyj <andreysivyj@gmail.com>
2024-08-26 02:30:58 +0300

    add list images and scroll event (Fetch API, ajax; CSRF, js-cookie)

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'images.apps.ImagesConfig',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required #предотвращаем доступ неаутентифицированных пользователей
@@ -30,3 +33,51 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user) # Если метод add() вызывается, передавая объект, который уже присутствует 
+                                                    # в наборе связанных объектов, то этот объект не будет дублироваться
+            else:
+                image.users_like.remove(request.user) # Если метод remove() вызывается с объектом, которого нет в наборе 
+                                                        # связанных объектов, то ничего не произойдет
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, то доставить первую страницу
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # Если AJAX-запрос и страница вне диапазона, то вернуть пустую страницу
+            return HttpResponse('')
+        # Если страница вне диапазона, то вернуть последнюю страницу результатов
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                   {'section': 'images',
+                    'images': images})
